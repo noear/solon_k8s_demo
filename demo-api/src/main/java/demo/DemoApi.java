3448fac233a189287ee4aab94ad0aaec34f934a4
noear <noear@live.cn>
2021-06-18 23:01:46 +0800

    1.4.5

---
+++ demo-api/src/main/java/demo/DemoApi.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.noear.solon.Solon;
+
+/**
+ * @author noear 2021/6/18 created
+ */
+public class DemoApi {
+    public static void main(String[] args) {
+        Solon.start(DemoApi.class, args).get("/", c->c.output("hello world!"));
+    }
+}
