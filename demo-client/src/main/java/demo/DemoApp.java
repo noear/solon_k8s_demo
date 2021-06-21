8119b89301cf26ff206f3988d9c41df062d58bdb
noear <noear@live.cn>
2021-06-21 11:27:09 +0800

    1.4.5

--- demo-client/src/main/java/demo/DemoApp.java
+++ demo-client/src/main/java/demo/DemoApp.java
@@ -1,12 +1,16 @@
 package demo;
 
 import org.noear.solon.Solon;
+import org.noear.solon.cloud.tool.HttpUtils;
 
 /**
  * @author noear 2021/6/18 created
  */
 public class DemoApp {
     public static void main(String[] args) {
-        Solon.start(DemoApp.class, args).get("/",c->c.output("hello world!"));
+        Solon.start(DemoApp.class, args).get("/",c->{
+            String rst = HttpUtils.http("http://demo-api/").get();
+            c.output(rst);
+        });
     }
 }
